86b544a3c94e088d189ff64ead3bc1160ac8549d
aaliahammedpriom <aaliahammedpriom66@gmail.com>
2025-02-19 02:25:53 +0600

    toggle work end  on home page

--- src/Layouts/Dashboard.jsx
+++ src/Layouts/Dashboard.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Navbar from '../Pages/Shared/Navbar';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Pages/Shared/Footer';
+import useAuth from '../Hooks/useAuth';
 
 const Dashboard = () => {
+    const {toggle}= useAuth()
     return (
-        <div data-theme="light" >
+        <div data-theme={toggle ? "dark" : "light"} >
             <Navbar></Navbar>
             <div className='min-h-screen pt-20'>
                 <Outlet></Outlet>
